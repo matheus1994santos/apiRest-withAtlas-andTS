a7c916867c0ba49ea284939c88bf5f6179abc393
Noronha <devmnoronha@gmail.com>
2025-05-07 13:38:11 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import express from "express";
+import { config } from "dotenv";
 
-console.log("Hello World, LOop");
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log("listen on port ", port, "!"));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
