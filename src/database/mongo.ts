34dba7e9a879598c68bb4b33244552b6d5e294e2
Noronha <devmnoronha@gmail.com>
2025-05-07 17:01:34 -0300

    feat: add MongoClient with mongodb connection

---
+++ src/database/mongo.ts
@@ -0,0 +1,23 @@
+import { MongoClient as Mongo, Db } from "mongodb";
+
+export const MongoClient = {
+  client: undefined as unknown as Mongo,
+  db: undefined as unknown as Db,
+
+  async connect(): Promise<void> {
+    const url = process.env.MONGODB_URL || "localhost:27017";
+    const username = process.env.MONGO_USERNAME;
+    const password = process.env.MONGO_PASSWORD;
+
+    const client = new Mongo(url, { auth: { username, password } });
+    const db = client.db("users-db");
+
+    this.client = client;
+    this.db = db;
+
+    console.log("connected to mongodb!");
+
+    // Para ler uma collection
+    // db.collection('users').find();
+  },
+};
